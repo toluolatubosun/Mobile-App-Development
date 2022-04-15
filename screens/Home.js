dae1a0e865da194094500bfdbe420c5526722077
John Olatubosun <toluolatubosun@gmail.com>
2022-04-15 21:03:52 +0100

    move between screens

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import { globalStyles } from '../styles/global';
 
-export default function Home() {
+export default function Home({ navigation }) {
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.titleText}>Home Page</Text>
+            <Button onPress={() => navigation.navigate("Note")} title="Go to notes"/>
+            {/* We also have push and pop */}
         </View>
     )
 }
