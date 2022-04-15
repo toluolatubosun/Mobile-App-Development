ea9f0a0d277031aba1f5becb0ba05cc745720b4d
John Olatubosun <toluolatubosun@gmail.com>
2022-04-15 19:50:36 +0100

    create screens

---
+++ screens/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from "react-native";
+
+export default function About() {
+    return (
+        <View style={globalStyles.container}>
+            <Text>About Page</Text>
+        </View>
+    )
+}
---
+++ screens/Note.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from "react-native";
+
+export default function Note() {
+    return (
+        <View style={globalStyles.container}>
+            <Text>Note Page</Text>
+        </View>
+    )
+}
